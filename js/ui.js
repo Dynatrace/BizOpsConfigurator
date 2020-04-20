947675366162f5fa98b4a6c6471786aa9667b488
Lucas Hocker <lucas.hocker@dynatrace.com>
2020-04-19 21:54:53 -0500

    downloadExcel

--- js/ui.js
+++ js/ui.js
@@ -72,13 +72,15 @@ function download(filename, text) {
 function downloadExcel(filename,worksheet,selector){
   let uri = 'data:application/vnd.ms-excel;base64,';
   let template = '<html xmlns:o="urn:schemas-microsoft-com:office:office" xmlns:x="urn:schemas-microsoft-com:office:excel" xmlns="http://www.w3.org/TR/REC-html40"><head><!--[if gte mso 9]><xml><x:ExcelWorkbook><x:ExcelWorksheets><x:ExcelWorksheet><x:Name>{worksheet}</x:Name><x:WorksheetOptions><x:DisplayGridlines/></x:WorksheetOptions></x:ExcelWorksheet></x:ExcelWorksheets></x:ExcelWorkbook></xml><![endif]--></head><body><table>{table}</table></body></html>';
-  let base64 = function (s) { return window.btoa(unescape(encodeURIComponent(s))) };
-  let format = function (s, c) { return s.replace(/{(\w+)}/g, function (m, p) { return c[p]; }) };
+  //let base64 = function (s) { return window.btoa(unescape(encodeURIComponent(s))) };
+  //let format = function (s, c) { return s.replace(/{(\w+)}/g, function (m, p) { return c[p]; }) };
   var ctx = { worksheet: worksheet, table: $(selector).html() }
+  let formatted = template.replace(/{(\w+)}/g, function (m, p) { return ctx[p]; })
+  let base64ed = window.btoa(unescape(encodeURIComponent(formatted)));
 
-  
   var element = document.createElement('a');
-  element.setAttribute('href', uri + base64(format(template, ctx)));
+  //element.setAttribute('href', uri + base64(format(template, ctx)));
+  element.setAttribute('href', uri + base64ed);
   element.setAttribute('download', filename);
 
   element.style.display = 'none';
