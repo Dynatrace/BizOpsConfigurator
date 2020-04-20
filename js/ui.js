14c306f849a254b9f35c8ced456e2d1193beffd7
Lucas Hocker <lucas.hocker@dynatrace.com>
2020-04-19 21:44:30 -0500

    html()

--- js/ui.js
+++ js/ui.js
@@ -74,7 +74,7 @@ function downloadExcel(filename,worksheet,selector){
   let template = '<html xmlns:o="urn:schemas-microsoft-com:office:office" xmlns:x="urn:schemas-microsoft-com:office:excel" xmlns="http://www.w3.org/TR/REC-html40"><head><!--[if gte mso 9]><xml><x:ExcelWorkbook><x:ExcelWorksheets><x:ExcelWorksheet><x:Name>{worksheet}</x:Name><x:WorksheetOptions><x:DisplayGridlines/></x:WorksheetOptions></x:ExcelWorksheet></x:ExcelWorksheets></x:ExcelWorkbook></xml><![endif]--></head><body><table>{table}</table></body></html>';
   let base64 = function (s) { return window.btoa(unescape(encodeURIComponent(s))) };
   let format = function (s, c) { return s.replace(/{(\w+)}/g, function (m, p) { return c[p]; }) };
-  var ctx = { worksheet: worksheet, table: $(selector).innerHTML }
+  var ctx = { worksheet: worksheet, table: $(selector).html() }
 
   
   var element = document.createElement('a');
