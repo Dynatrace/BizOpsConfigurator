fc677e0f811fbb89ae3804405b0f62876023d8cc
Lucas Hocker <lucas.hocker@dynatrace.com>
2020-05-02 21:37:21 -0500

    switch to single quote

--- js/inputChangeHandlers.js
+++ js/inputChangeHandlers.js
@@ -364,7 +364,7 @@ function tenantOverviewChangeHandler() {
         let customapps = apps.filter(app => app.entityId.includes("CUSTOM_APPLICATION"));
         let html = "";
         customapps.forEach(function(app){
-          html += `<option value="${JSON.stringify(app)}">${app.displayName}</option>`;
+          html += `<option value='${JSON.stringify(app)}'>${app.displayName}</option>`;
         });
         $("#SAPapps").html(html);
       });
