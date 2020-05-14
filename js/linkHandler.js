96d544ec260fa49e957d79b1fae51a57e6700d46
Lucas Hocker <lucas.hocker@dynatrace.com>
2020-05-14 17:44:27 -0500

    jquery traversing

--- js/linkHandler.js
+++ js/linkHandler.js
@@ -166,7 +166,7 @@ function linkHandler(e) {
         break;
       }
       case "readmeIcon": {
-        let overview = $(this).next("select").val();
+        let overview = $(this).parent().next().children("select").val();
         let overviewRepo = dbList.find(({ name }) => name === overview).repo; //get the repo directly from the select in next iteration
         let readmes = dbList.filter(({name}) => name === "README.md");
         let readme = readmes.find(({ repo }) => repo.owner === overviewRepo.owner &&
