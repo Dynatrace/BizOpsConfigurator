a8e8b0c02c696a1e34ef23330005f58435816c9b
Lucas Hocker <lucas.hocker@dynatrace.com>
2020-05-17 12:54:48 -0500

    workflowGenerator

--- js/globalButtonHandler.js
+++ js/globalButtonHandler.js
@@ -841,6 +841,10 @@ function globalButtonHandler() {
         $("#viewport").load("html/personaFlow/persona_userInputs.html", fieldsetPainter);
         break;
       }
+      case "workflowGenerator":{
+        $("#viewport").load("html/personaFlow/workflowBuilder.html", fieldsetPainter);
+        break;
+      }
       case "":
       case undefined:
         console.log("undefined button");
