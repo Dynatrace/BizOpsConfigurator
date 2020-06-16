2f1da82edbcb9e5b449e9853e10248d7c0d90756
Lucas Hocker <lucas.hocker@dynatrace.com>
2020-06-16 16:24:33 -0500

    gitHubAPI

--- js/githubAPI.js
+++ js/githubAPI.js
@@ -3,7 +3,7 @@ Licensed under the Apache License, Version 2.0 (the "License"); you may not use 
 http://www.apache.org/licenses/LICENSE-2.0
 Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.*/
 function getRepoContents(repo) {
-    return gitHubAPI(`${repo.owner}/${repo.repo}/contents/${repo.path}`);
+    return gitHubAPI(`/repos/${repo.owner}/${repo.repo}/contents/${repo.path}`);
 }
 
 function gitHubAPI(url,options={},retries=3) {
@@ -13,7 +13,7 @@ function gitHubAPI(url,options={},retries=3) {
     
     //Get App list from API as JSON
     return $.ajax({
-    url: "https://api.github.com/repos/"+repo.owner+"/"+repo.repo+"/contents/"+repo.path,
+    url: "https://api.github.com/"+url,
     contentType: "application/json; charset=utf-8",
     method: 'GET',
     dataType: "json",
