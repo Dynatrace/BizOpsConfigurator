28904e918e427ba84fc3cca6d63041562bfbce23
Lucas Hocker <lucas.hocker@dynatrace.com>
2020-06-16 17:17:39 -0500

    github ratelimiting cors failures

--- js/githubAPI.js
+++ js/githubAPI.js
@@ -6,102 +6,107 @@ function getRepoContents(repo) {
     return gitHubAPI(`/repos/${repo.owner}/${repo.repo}/contents/${repo.path}`);
 }
 
-function gitHubAPI(url,options={},retries=3) {
+function gitHubAPI(query, options = {}, retries = 3) {
     let headers = (typeof options.headers != "undefined") ? options.headers : {};
-    if(githubuser!="" && githubpat!="")
-        headers.Authorization = "Basic " + btoa(githubuser+":"+githubpat);
-    
-    //Get App list from API as JSON
-    return $.ajax({
-    url: "https://api.github.com"+url,
-    contentType: "application/json; charset=utf-8",
-    method: 'GET',
-    dataType: "json",
-    headers: headers
-    })
-    //.fail(errorboxJQXHR);
-    .fail(function (jqXHR, textStatus, errorThrown) {
-        if (jqXHR.getResponseHeader("X-RateLimit-Remaining") !== 0) return; //only handle ratelimiting here for now
-        if (retries <= 0) {
-          errorboxJQXHR(jqXHR, "Retries exhausted.", errorThrown);
-          return;
-        }
-        let seconds = 0;
-        let now = 0;
-        let then = 0;
-        try {
-          then = parseInt(jqXHR.getResponseHeader("X-Ratelimit-Reset"));
-          now = (new Date().getTime())/1000;
-          seconds = (then - now) + 1;
-        } catch (e) { seconds = 60; } //if we didn't capture the reset time, just default to a minute
-        warningbox(`GitHub API Ratelimiting: retrying in ${seconds}s. Consider using GitHub PAT to avoid this.`);
-        console.log("Inside Fail: query=" + query + " retries=" + retries + " seconds=" + seconds + " now=" + now + " then=" + then);
-        return setTimeout(function () { gitHubAPI(query, options, retries - 1); }, seconds * 1000);
-      });
+    if (githubuser != "" && githubpat != "")
+        headers.Authorization = "Basic " + btoa(githubuser + ":" + githubpat);
+
+    let url = "https://api.github.com" + query;
+    if (Object.keys(headers) === 0) { //no need for preflight
+        return $.ajax(url)
+            .fail(gitHubAPIFailHandler);
+    } else { //fine to do full preflight
+        return $.ajax({
+            url: query,
+            contentType: "application/json; charset=utf-8",
+            method: 'GET',
+            dataType: "json",
+            headers: headers
+        })
+            .fail(gitHubAPIFailHandler);
+    }
 }
 
+function gitHubAPIFailHandler(jqXHR, textStatus, errorThrown) {
+    if (jqXHR.getResponseHeader("X-RateLimit-Remaining") !== 0) return; //only handle ratelimiting here for now
+    if (retries <= 0) {
+        errorboxJQXHR(jqXHR, "Retries exhausted.", errorThrown);
+        return;
+    }
+    let seconds = 0;
+    let now = 0;
+    let then = 0;
+    try {
+        then = parseInt(jqXHR.getResponseHeader("X-Ratelimit-Reset"));
+        now = (new Date().getTime()) / 1000;
+        seconds = (then - now) + 1;
+    } catch (e) { seconds = 60; } //if we didn't capture the reset time, just default to a minute
+    warningbox(`GitHub API Ratelimiting: retrying in ${seconds}s. Consider using GitHub PAT to avoid this.`);
+    console.log("Inside Fail: query=" + query + " retries=" + retries + " seconds=" + seconds + " now=" + now + " then=" + then);
+    return setTimeout(function () { gitHubAPI(query, options, retries - 1); }, seconds * 1000);
+}
 
-function getREADME(repo){ //not used anymore
-    let headers = {'Accept':'application/vnd.github.v3.html'};
-    if(githubuser!="" && githubpat!="")
-        headers.Authorization = "Basic " + btoa(githubuser+":"+githubpat);
+function getREADME(repo) { //not used anymore
+    let headers = { 'Accept': 'application/vnd.github.v3.html' };
+    if (githubuser != "" && githubpat != "")
+        headers.Authorization = "Basic " + btoa(githubuser + ":" + githubpat);
 
     //Get README.md as HTML
     return $.get({
-    url: `https://api.github.com/repos/${repo.owner}/${repo.repo}/readme`,
-    headers: headers
+        url: `https://api.github.com/repos/${repo.owner}/${repo.repo}/readme`,
+        headers: headers
     })
-    .fail(errorboxJQXHR);
+        .fail(errorboxJQXHR);
 }
 
-function parseRepoContents(data,repo) {
+function parseRepoContents(data, repo) {
     let dbListTemp = [];
     let workflowsTemp = [];
     let readmesTemp = [];
-    data.forEach(function(file) {
-        file.repo=repo;
+    data.forEach(function (file) {
+        file.repo = repo;
         let reWorkflow = /(\.cwf\.json$)/;
         let reDB = /(\.json$)|(^[0-9a-f-]{36}$)/;
         let reReadme = /\.md$/;
-        if(reWorkflow.test(file.name)){
+        if (reWorkflow.test(file.name)) {
             workflowsTemp.push(file);
-        } else if(reDB.test(file.name)) {
+        } else if (reDB.test(file.name)) {
             dbListTemp.push(file);
-        } else if(reReadme.test(file.name)) {
+        } else if (reReadme.test(file.name)) {
             readmesTemp.push(file);
         } else {
-            console.log("parseRepoContents: rejected '"+file.path+"' based on regex");
+            console.log("parseRepoContents: rejected '" + file.path + "' based on regex");
         }
     });
-    return {dbList:dbListTemp, workflowList: workflowsTemp, readmeList: readmesTemp};
+    return { dbList: dbListTemp, workflowList: workflowsTemp, readmeList: readmesTemp };
 }
 
 function getDBJSON(list) {
     let dbs = [];
     let promises = [];
 
-    list.forEach(function(file) {
+    list.forEach(function (file) {
         let p = $.get(file.download_url)
             .fail(errorboxJQXHR);
         promises.push(p);
     });
 
-    return $.when.apply($, promises).then(function() {
-        for(let i=0; i<arguments.length; i++) {
+    return $.when.apply($, promises).then(function () {
+        for (let i = 0; i < arguments.length; i++) {
             dbs.push(JSON.parse(arguments[i][0]));;
         };
         return dbs;
     });
 }
 
-function testRepo(i=0) {
+function testRepo(i = 0) {
     let p1 = getRepoContents(repoList[i]);
 
-    $.when(p1).done(function(data) {
-        let repos = parseRepoContents(data,repoList[i]);
+    $.when(p1).done(function (data) {
+        let repos = parseRepoContents(data, repoList[i]);
 
         let p2 = getDBJSON(repos);
-        $.when(p2).done(function(d2) {
+        $.when(p2).done(function (d2) {
             console.log(d2);
         });
     });
