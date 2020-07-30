0e051f1c53d3ed2f0bb8b82522128bef3c25afeb
Lucas Hocker <lucas.hocker@dynatrace.com>
2020-07-30 15:42:30 -0500

    handle nulls in app forecast

--- js/forecast.js
+++ js/forecast.js
@@ -15,74 +15,74 @@ function generateFunnelForecast(config) {
     var dateTemp = new Date();
     var startdate = "";
     var enddate = "";
-    var filterClause = ("filterClause" in config)?config.filterClause:"";
-    const week = 1000*60*60*24*7;
- 
-    for(let i=1; i<=5; i++) {
-        let queries=[];
-        dateTemp.setTime(date.getTime() - i*week);
+    var filterClause = ("filterClause" in config) ? config.filterClause : "";
+    const week = 1000 * 60 * 60 * 24 * 7;
+
+    for (let i = 1; i <= 5; i++) {
+        let queries = [];
+        dateTemp.setTime(date.getTime() - i * week);
         startdate = dateTemp.getTime();
 
-        dateTemp.setTime(date.getTime() - (i-1)*week)
+        dateTemp.setTime(date.getTime() - (i - 1) * week)
         enddate = dateTemp.getTime();
 
-        if(revenue!="n/a"){
+        if (revenue != "n/a") {
             //Revenue
-            queries.push('select sum('+revenue+') as "rev'+i+'", avg('+revenue+') as "acv'+i+'" from usersession where useraction.application="'+appname+'" and '+laststep+filterClause+' and startTime between '+startdate+' and '+enddate);
+            queries.push('select sum(' + revenue + ') as "rev' + i + '", avg(' + revenue + ') as "acv' + i + '" from usersession where useraction.application="' + appname + '" and ' + laststep + filterClause + ' and startTime between ' + startdate + ' and ' + enddate);
 
             //Risk Revenue
-            queries.push('select sum('+revenue+') as "riskrev'+i+'", avg('+revenue+') as "arcv'+i+'" from usersession where useraction.application="'+appname+'" and userExperienceScore!="SATISFIED" and '+laststep+filterClause+' and startTime between '+startdate+' and '+enddate);
+            queries.push('select sum(' + revenue + ') as "riskrev' + i + '", avg(' + revenue + ') as "arcv' + i + '" from usersession where useraction.application="' + appname + '" and userExperienceScore!="SATISFIED" and ' + laststep + filterClause + ' and startTime between ' + startdate + ' and ' + enddate);
 
             //Lost Revenue
-            queries.push('select sum('+revenue+') as "lostrev'+i+'", avg('+revenue+') as "alcv'+i+'" from usersession where useraction.application="'+appname+'" and not '+laststep+filterClause+' and startTime between '+startdate+' and '+enddate);
+            queries.push('select sum(' + revenue + ') as "lostrev' + i + '", avg(' + revenue + ') as "alcv' + i + '" from usersession where useraction.application="' + appname + '" and not ' + laststep + filterClause + ' and startTime between ' + startdate + ' and ' + enddate);
         }
-        
+
         //Funnel Forecasting
         //Conversions
-        queries.push('select count(usersessionid) as "funconv'+i+'" from usersession where useraction.application="'+appname+'" and '+laststep+filterClause+' and startTime between '+startdate+' and '+enddate);
+        queries.push('select count(usersessionid) as "funconv' + i + '" from usersession where useraction.application="' + appname + '" and ' + laststep + filterClause + ' and startTime between ' + startdate + ' and ' + enddate);
         //Abandons
-        queries.push('select count(usersessionid) as "funaban'+i+'" from usersession where useraction.application="'+appname+'" and not '+laststep+filterClause+' and startTime between '+startdate+' and '+enddate);
+        queries.push('select count(usersessionid) as "funaban' + i + '" from usersession where useraction.application="' + appname + '" and not ' + laststep + filterClause + ' and startTime between ' + startdate + ' and ' + enddate);
         //Funnel Visits
-        queries.push('select count(usersessionid) as "funvisit'+i+'" from usersession where useraction.application="'+appname+'" and '+firststep+filterClause+' and startTime between '+startdate+' and '+enddate);
+        queries.push('select count(usersessionid) as "funvisit' + i + '" from usersession where useraction.application="' + appname + '" and ' + firststep + filterClause + ' and startTime between ' + startdate + ' and ' + enddate);
         //Number of Satisfied Users
-        queries.push('select count(usersessionid) as "fnosu'+i+'" from usersession where useraction.application="'+appname+'" and userExperienceScore="SATISFIED" and '+firststep+filterClause+' and startTime between '+startdate+' and '+enddate);
+        queries.push('select count(usersessionid) as "fnosu' + i + '" from usersession where useraction.application="' + appname + '" and userExperienceScore="SATISFIED" and ' + firststep + filterClause + ' and startTime between ' + startdate + ' and ' + enddate);
         //Session Duration
-        queries.push('select avg(duration) as "fundur'+i+'" from usersession where useraction.application="'+appname+'" and '+firststep+filterClause+' and startTime between '+startdate+' and '+enddate);
+        queries.push('select avg(duration) as "fundur' + i + '" from usersession where useraction.application="' + appname + '" and ' + firststep + filterClause + ' and startTime between ' + startdate + ' and ' + enddate);
 
-        queries.forEach(function(usql) {
-            var query="/api/v1/userSessionQueryLanguage/table?query="+encodeURIComponent(usql)+"&startTimestamp=1000000000000&explain=false";
-            deferred = dtAPIquery(query,{})
-                $.when(deferred).done(function(d) {
-                    d.columnNames.forEach(function(c,i,a) {
-                        revs.push({from:c, to:d.values[0][i]});
-                    });
+        queries.forEach(function (usql) {
+            var query = "/api/v1/userSessionQueryLanguage/table?query=" + encodeURIComponent(usql) + "&startTimestamp=1000000000000&explain=false";
+            deferred = dtAPIquery(query, {})
+            $.when(deferred).done(function (d) {
+                d.columnNames.forEach(function (c, i, a) {
+                    revs.push({ from: c, to: d.values[0][i] });
                 });
+            });
             deferreds.push(deferred);
         });
     }
-    $.when.apply($, deferreds).then(function(d) {
-       revs.sort((a, b) => (a.from.length < b.from.length) ? 1 : -1); 
-       console.log(revs); 
-       main.resolve(revs);
+    $.when.apply($, deferreds).then(function (d) {
+        revs.sort((a, b) => (a.from.length < b.from.length) ? 1 : -1);
+        console.log(revs);
+        main.resolve(revs);
     });
     return main;
 }
 
-function updateFunnelForecast(config,ov,revs) {
+function updateFunnelForecast(config, ov, revs) {
     let swaps = generateFunnelSwapList(config);
-    swaps=swaps.concat(revs);
-    swaps.push({from:'tfactor', to:config.tfactor});
-    swaps.push({from:'tdate', to:new Date().toDateString()});
+    swaps = swaps.concat(revs);
+    swaps.push({ from: 'tfactor', to: config.tfactor });
+    swaps.push({ from: 'tdate', to: new Date().toDateString() });
     let deferreds = [];
     let subs = [];
-    let re = new RegExp(ov.substring(0,24));
+    let re = new RegExp(ov.substring(0, 24));
 
-    dbList.forEach(function(d) { 
-        if(d.name.includes("Forecast")) {
-            if(d.name.includes("Revenue") && (!("kpi" in config) || config.kpi=='n/a')) return;
+    dbList.forEach(function (d) {
+        if (d.name.includes("Forecast")) {
+            if (d.name.includes("Revenue") && (!("kpi" in config) || config.kpi == 'n/a')) return;
 
-            config.subids.forEach(function(i) {
-                if(d.file.id == i.from) {
+            config.subids.forEach(function (i) {
+                if (d.file.id == i.from) {
                     subs.push(JSON.parse(JSON.stringify(d)));
                     swaps.push(i);
                 }
@@ -90,32 +90,32 @@ function updateFunnelForecast(config,ov,revs) {
         }
     });
 
-    subs.forEach(function(s,i,arr) {
-        s.file["dashboardMetadata"]["owner"]=owner;
-        s.file["dashboardMetadata"]["shared"]="true";
-        s.file["dashboardMetadata"]["sharingDetails"]["linkShared"]="true";
-        s.file["dashboardMetadata"]["sharingDetails"]["published"]="false";
-        if("costControlUserSessionPercentage" in config) addCostControlTile(s.file,config);
-        addReplaceButton(s.file,config.FOid,"![BackButton]()","⇦",findTopRight);
+    subs.forEach(function (s, i, arr) {
+        s.file["dashboardMetadata"]["owner"] = owner;
+        s.file["dashboardMetadata"]["shared"] = "true";
+        s.file["dashboardMetadata"]["sharingDetails"]["linkShared"] = "true";
+        s.file["dashboardMetadata"]["sharingDetails"]["published"] = "false";
+        if ("costControlUserSessionPercentage" in config) addCostControlTile(s.file, config);
+        addReplaceButton(s.file, config.FOid, "![BackButton]()", "⇦", findTopRight);
         arr[i].file = doSwaps(JSON.stringify(s.file), swaps);
         //arr[i].file = JSON.parse(swapped);
     });
     return uploadSubs(subs);
 }
 
-function updateAppForecast(config,ov,revs) {
+function updateAppForecast(config, ov, revs) {
     let swaps = generateAppSwapList(config);
-    swaps=swaps.concat(revs);
-    swaps.push({from:'tfactor', to:config.tfactor});
-    swaps.push({from:'tdate', to:new Date().toDateString()});
+    swaps = swaps.concat(revs);
+    swaps.push({ from: 'tfactor', to: config.tfactor });
+    swaps.push({ from: 'tdate', to: new Date().toDateString() });
     let deferreds = [];
     let subs = [];
-    let re = new RegExp(ov.substring(0,24));
+    let re = new RegExp(ov.substring(0, 24));
 
-    dbList.forEach(function(d) { 
-        if(d.name.includes("Forecast")) {
-            config.subids.forEach(function(i) {
-                if(d.file.id == i.from) {
+    dbList.forEach(function (d) {
+        if (d.name.includes("Forecast")) {
+            config.subids.forEach(function (i) {
+                if (d.file.id == i.from) {
                     subs.push(JSON.parse(JSON.stringify(d)));
                     swaps.push(i);
                 }
@@ -123,13 +123,13 @@ function updateAppForecast(config,ov,revs) {
         }
     });
 
-    subs.forEach(function(s,i,arr) {
-        s.file["dashboardMetadata"]["owner"]=owner;
-        s.file["dashboardMetadata"]["shared"]="true";
-        s.file["dashboardMetadata"]["sharingDetails"]["linkShared"]="true";
-        s.file["dashboardMetadata"]["sharingDetails"]["published"]="false";
-        if("costControlUserSessionPercentage" in config) addCostControlTile(s.file,config);
-        addReplaceButton(s.file,config.AOid,"![BackButton]()","⇦",findTopRight);
+    subs.forEach(function (s, i, arr) {
+        s.file["dashboardMetadata"]["owner"] = owner;
+        s.file["dashboardMetadata"]["shared"] = "true";
+        s.file["dashboardMetadata"]["sharingDetails"]["linkShared"] = "true";
+        s.file["dashboardMetadata"]["sharingDetails"]["published"] = "false";
+        if ("costControlUserSessionPercentage" in config) addCostControlTile(s.file, config);
+        addReplaceButton(s.file, config.AOid, "![BackButton]()", "⇦", findTopRight);
         arr[i].file = doSwaps(JSON.stringify(s.file), swaps);
         //arr[i].file = JSON.parse(swapped);
     });
@@ -146,44 +146,46 @@ function generateAppForecast(config) {
     var dateTemp = new Date();
     var startdate = "";
     var enddate = "";
-    const week = 1000*60*60*24*7;
- 
-    for(let i=1; i<=5; i++) {
-        let queries=[];
-        dateTemp.setTime(date.getTime() - i*week);
+    const week = 1000 * 60 * 60 * 24 * 7;
+
+    for (let i = 1; i <= 5; i++) {
+        let queries = [];
+        dateTemp.setTime(date.getTime() - i * week);
         startdate = dateTemp.getTime();
-//console.log("startdate: "+dateTemp.toDateString());
-        dateTemp.setTime(date.getTime() - (i-1)*week)
+        //console.log("startdate: "+dateTemp.toDateString());
+        dateTemp.setTime(date.getTime() - (i - 1) * week)
         enddate = dateTemp.getTime();
-//console.log("endDate: " + dateTemp.toDateString());
+        //console.log("endDate: " + dateTemp.toDateString());
         // Application Forecasting
         // Number of Satisfied Users
-        queries.push('select count(usersessionid) as "anosu'+i+'" from usersession where useraction.application="'+appname+'" and userExperienceScore="SATISFIED" and startTime between '+startdate+' and '+enddate);
+        queries.push('select count(usersessionid) as "anosu' + i + '" from usersession where useraction.application="' + appname + '" and userExperienceScore="SATISFIED" and startTime between ' + startdate + ' and ' + enddate);
         // Number of Tolerated Users
-        queries.push('select count(usersessionid) as "anotu'+i+'" from usersession where useraction.application="'+appname+'" and userExperienceScore="TOLERATED" and startTime between '+startdate+' and '+enddate);
+        queries.push('select count(usersessionid) as "anotu' + i + '" from usersession where useraction.application="' + appname + '" and userExperienceScore="TOLERATED" and startTime between ' + startdate + ' and ' + enddate);
         // Number of Frustrated Users
-        queries.push('select count(usersessionid) as "anofu'+i+'" from usersession where useraction.application="'+appname+'" and userExperienceScore="FRUSTRATED" and startTime between '+startdate+' and '+enddate);
+        queries.push('select count(usersessionid) as "anofu' + i + '" from usersession where useraction.application="' + appname + '" and userExperienceScore="FRUSTRATED" and startTime between ' + startdate + ' and ' + enddate);
         // Action Duration
-        queries.push('select avg(useraction.duration) as "appdur'+i+'", count(useraction.errorCount) as "apperr'+i+'" from usersession where useraction.application="'+appname+'" and useraction.keyUserAction=true and startTime between '+startdate+' and '+enddate);
+        queries.push('select avg(useraction.duration) as "appdur' + i + '", count(useraction.errorCount) as "apperr' + i + '" from usersession where useraction.application="' + appname + '" and useraction.keyUserAction=true and startTime between ' + startdate + ' and ' + enddate);
         // Action Errors
         //queries.push('select count(useraction.errorCount) as "apperr'+i+'" from usersession where useraction.application="'+appname+'" and startTime between '+startdate+' and '+enddate);
 
-        queries.forEach(function(usql) {
-//console.log(usql);
-            var query="/api/v1/userSessionQueryLanguage/table?query="+encodeURIComponent(usql)+"&startTimestamp=1000000000000&explain=false";
-            deferred = dtAPIquery(query,{})
-                $.when(deferred).done(function(d) {
-                    d.columnNames.forEach(function(c,i,a) {
-                        revs.push({from:c, to:d.values[0][i]});
-                    });
+        queries.forEach(function (usql) {
+            //console.log(usql);
+            var query = "/api/v1/userSessionQueryLanguage/table?query=" + encodeURIComponent(usql) + "&startTimestamp=1000000000000&explain=false";
+            deferred = dtAPIquery(query, {})
+            $.when(deferred).done(function (d) {
+                d.columnNames.forEach(function (c, i, a) {
+                    let val = d.values[0][i];
+                    if (val == "null" || val == null) val = 0;
+                    revs.push({ from: c, to: val });
                 });
+            });
             deferreds.push(deferred);
         });
     }
-    $.when.apply($, deferreds).then(function(d) {
-       revs.sort((a, b) => (a.from.length < b.from.length) ? 1 : -1); 
-       console.log(revs); 
-       main.resolve(revs);
+    $.when.apply($, deferreds).then(function (d) {
+        revs.sort((a, b) => (a.from.length < b.from.length) ? 1 : -1);
+        console.log(revs);
+        main.resolve(revs);
     });
     return main;
 }
