Dynatrace/BizOpsConfigurator

commit 5f06940107c63fbaee6538fade8a3367f5881b17

work on Edit mode

js/workflowBuilder/workflowBuilder-api.js
@@ -16,6 +16,20 @@ function loadApiQuery($query) {
     let p1 = loadApiQueryOptions(query, slicer, $target);
     return $.when(p1).done(function (data) {
         jsonviewer(data);
+
+        //if we already have the value, set it, e.g. if this is an edit
+        let transform = $query.parents(`.workflowInput`).find(`.transform span`).text();
+        if (selection
+            && Array.isArray(swaps)
+            && selection.swaps.length) {
+            let swaps = selection.swaps
+                .filter(x => x.from.endsWith('.id'))
+                .filter(x => x.from.replace(/\.id/, '') === transform);
+            if (Array.isArray(swaps)
+                && swaps.length) {
+                $target.value(swaps[0].to);
+            }
+        }
     });
 }
 

js/workflowBuilder/workflowBuilder-usql.js
@@ -20,6 +20,20 @@ function loadUsqlQuery($usql) {
     let p1 = loadUsqlQueryOptions(query, slicer, $target, whereClause, multiple);
     return $.when(p1).done(function (data) {
         jsonviewer(data);
+
+        //if we already have the value, set it, e.g. if this is an edit
+        let transform = $usql.parents(`.workflowInput`).find(`.transform span`).text();
+        if (selection
+            && Array.isArray(swaps)
+            && selection.swaps.length) {
+            let swaps = selection.swaps
+                .filter(x => x.from.endsWith('.id'))
+                .filter(x => x.from.replace(/\.id/, '') === transform);
+            if (Array.isArray(swaps)
+                && swaps.length) {
+                $target.value(swaps[0].to);
+            }
+        }
     });
 }
 

js/workflowBuilder/workflowBuilder-render.js
@@ -27,6 +27,7 @@ function renderWorkflow(el) {
         let val = $(fromInputs[i]).val();
         $(toInputs[i]).attr("value", val);
     }
+    
 
     //cleanup clone
     clonedEl.find(".chosen-select").chosen("destroy"); //in case we're already rendered for some reason

commit 89483003c16f9c206d4c111fdfd4f8d2ce4a5249

fix bug introduced in 5f069401

js/workflowBuilder/workflowBuilder-api.js
@@ -20,7 +20,7 @@ function loadApiQuery($query) {
         //if we already have the value, set it, e.g. if this is an edit
         let transform = $query.parents(`.workflowInput`).find(`.transform span`).text();
         if (selection
-            && Array.isArray(swaps)
+            && Array.isArray(selection.swaps)
             && selection.swaps.length) {
             let swaps = selection.swaps
                 .filter(x => x.from.endsWith('.id'))

js/workflowBuilder/workflowBuilder-usql.js
@@ -24,7 +24,7 @@ function loadUsqlQuery($usql) {
         //if we already have the value, set it, e.g. if this is an edit
         let transform = $usql.parents(`.workflowInput`).find(`.transform span`).text();
         if (selection
-            && Array.isArray(swaps)
+            && Array.isArray(selection.swaps)
             && selection.swaps.length) {
             let swaps = selection.swaps
                 .filter(x => x.from.endsWith('.id'))