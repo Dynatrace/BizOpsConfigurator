e79c54451ef7459a60b5ad68a5b2114568e0cba7
Lucas Hocker <lucas.hocker@dynatrace.com>
2021-06-22 16:13:54 -0500

    typo ##MZ-tabs

--- js/miscTools/mzcleanup.js
+++ js/miscTools/mzcleanup.js
@@ -162,7 +162,7 @@ async function runMZcleanupReport() {
     }
 
     function listOfEmptyMZs() {
-        $(`##MZ-tabs`).children().removeClass('active');
+        $(`#MZ-tabs`).children().removeClass('active');
         $(`#MZ-tab-empty`).parent().addClass('active');
         $resultbox.html(`<h2>Empty MZs:</h2>`);
         let $ul = $(`<ul>`).appendTo($resultbox);
@@ -174,7 +174,7 @@ async function runMZcleanupReport() {
     }
 
     function listDupMZs() {
-        $(`##MZ-tabs`).children().removeClass('active');
+        $(`#MZ-tabs`).children().removeClass('active');
         $(`#MZ-tab-dup`).parent().addClass('active');
         $resultbox.html(`<h2>Duplicate MZs:</h2>`);
         let $ul = $(`<ul>`).appendTo($resultbox);
@@ -194,18 +194,18 @@ async function runMZcleanupReport() {
     }
     
     function listFrequentRules() { 
-        $(`##MZ-tabs`).children().removeClass('active');
+        $(`#MZ-tabs`).children().removeClass('active');
         $(`#MZ-tab-rules`).parent().addClass('active');
         $resultbox.html(`<h2>Frequent Rules:</h2>`);
     }
     function listUnusedMZs() { 
-        $(`##MZ-tabs`).children().removeClass('active');
+        $(`#MZ-tabs`).children().removeClass('active');
         $(`#MZ-tab-unused`).parent().addClass('active');
         $resultbox.html(`<h2>Unused MZs:</h2>`);
     }
 
     function showJSON() {
-        $(`##MZ-tabs`).children().removeClass('active');
+        $(`#MZ-tabs`).children().removeClass('active');
         $(`#MZ-tab-json`).parent().addClass('active');
         $resultbox.html(`<h2>Full JSON Result:</h2>`);
         $resultbox.append(`<pre>`
