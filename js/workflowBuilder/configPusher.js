e7d0ac78ed2dcd9bd2b6175b41985c19ce0087f5
Lucas Hocker <lucas.hocker@dynatrace.com>
2021-08-10 15:57:11 -0500

    tsmMetricKey

--- js/workflowBuilder/configPusher.js
+++ js/workflowBuilder/configPusher.js
@@ -120,7 +120,8 @@ function ConfigPusherFactory(target, transform, configPushType, configPushFile, 
                 let query = `/api/config/v1/calculatedMetrics/${customMetricType}`;
                 p = dtAPIquery(query);
                 $.when(p).done(function (result) {
-                    if (result.values.find(x => x.id === c.metricKey && x.name === c.name)) {
+                    let MK = (c.hasOwnProperty("tsmMetricKey")?"tsmMetricKey":"metricKey");
+                    if (result.values.find(x => x.id === c[MK] && x.name === c.name)) {
                         configured = true;
                     } else {
                         configured = false;
