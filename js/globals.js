e48f8d3af69c18f72f70e089f7dbb3e0d699095a
Lucas Hocker <lucas.hocker@dynatrace.com>
2021-10-28 13:18:32 -0500

    correct s3 url

--- js/globals.js
+++ js/globals.js
@@ -3,7 +3,7 @@ Licensed under the Apache License, Version 2.0 (the "License"); you may not use 
 http://www.apache.org/licenses/LICENSE-2.0
 Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.*/
 //////// Constants //////////////
-const s3URL = `https://bizopsconfigurator.s3.amazonaws.com/dashboardpack-latest.json.gz`;
+const s3URL = `https://bizopsconfigurator.s3.amazonaws.com/dashboardpack-latest.json`;
 var configDashboard = "json/configDashboard.json";
 const REpersonaWorkflow = /^bbbbbbbb-a[0-9]{3}-a[0-9]{3}-[0]{4}-[0-9]{12}$/;
 const unique = (value, index, self) => self.indexOf(value) === index;
